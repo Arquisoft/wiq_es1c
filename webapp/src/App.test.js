414cef02e4fe6f3020b521467e74d80d0c1ae153
Jose Emilio Labra Gayo <jelabra@gmail.com>
2024-01-26 09:41:33 +0100

    Changed test to app

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the 2024 edition of the Software Architecture course/i);
+  const linkElement = screen.getByText(/Welcome to wiq_es1c/i);
   expect(linkElement).toBeInTheDocument();
 });
