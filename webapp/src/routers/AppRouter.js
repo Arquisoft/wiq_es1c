7f1a12652eab74f4269ebbb92508f65f71a43268
NoelEE <uo289307@uniovi.es>
2024-02-20 14:57:29 +0100

    Created Home.js and added very basic functionality

--- webapp/src/routers/AppRouter.js
+++ webapp/src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "../components/login/Login";
 import AddUser from "../components/register/AddUser";
 import Error404 from "../components/error/Error404";
+import Home from "../components/home/Home"
 
 const router = createBrowserRouter([
     {
@@ -11,7 +12,7 @@ const router = createBrowserRouter([
     },
     {
         path: "/home",
-        element: <h1 className="text-white text-5xl text-center">Pantalla del juego</h1>
+        element: <Home />
     },
     {
         path: "/login",
