7420f45963522d150348977b397588c4226deb53
Yago <uo289549@uniovi.es>
2024-02-26 17:23:42 +0100

    creacion componente game

---
+++ webapp/src/components/game/Game.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Game = () => {
+  return (
+    <div>Game</div>
+  )
+}
--- webapp/src/routers/AppRouter.js
+++ webapp/src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "../components/login/Login";
 import AddUser from "../components/register/AddUser";
 import Error404 from "../components/error/Error404";
+import Game from "../components/game/Game";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <AddUser />
+    },
+    {
+        path: "/game",
+        element: <Game />
     },
     {
         path: "*",
