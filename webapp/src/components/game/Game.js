85968211d2e7f8528a325b1f80ed7d6ab4c5eff8
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-02-28 17:13:00 +0100

    Fixed question buttons

--- webapp/src/components/game/Game.js
+++ webapp/src/components/game/Game.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import {Button, Box, Container, CssBaseline,Typography,} from "@mui/material";
+import {Button, Box, Container, CssBaseline,Typography, Grid, Paper,} from "@mui/material";
 import './Game.css';
 import { startNewGame, nextQuestion, awnser } from "../../services/game.service";
 
@@ -8,8 +8,6 @@ export const Game = () => {
 
     const [pregunta, setPregunta] = useState("Cargando pregunta...");
     const [respuestas, setRespuestas] = useState(["...","...","...","..."]);
-
-    const boxRefs = useRef([]);
 
     const startGame = () => {
         startNewGame(token).then(() =>
@@ -20,17 +18,7 @@ export const Game = () => {
         );
     };
 
-    const comprobarPregunta = () => {
-
-        let respuesta = null;
-
-        boxRefs.current.forEach((boxRef, indice) => {
-            if(boxRef.dataset.state === "selected"){
-                respuesta = boxRef.dataset.question;
-                boxRef.dataset.state = "unselected";
-            }
-        });
-
+    const comprobarPregunta = (respuesta) => {
         awnser(token, respuesta).then((correcta) => {
 
             if(respuesta == correcta){
@@ -44,14 +32,6 @@ export const Game = () => {
                 setRespuestas(respuesta.awnsers);
             });
         })
-    };
-
-    const seleccionarRespuesta = (element) => {
-        boxRefs.current.forEach((boxRef, indice) => {
-            boxRef.dataset.state = "unselected";
-        });
-        
-        element.target.dataset.state = "selected";
     };
 
     //Call start only once
@@ -82,53 +62,32 @@ export const Game = () => {
                     {pregunta}
                 </Typography>
             </Box>
-            <Box sx={{
-                    padding: 1,
+
+            <Box
+                sx={{
+                    padding: 3,
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
                 }}
             >
-                <ul>
+                <Grid container spacing={2}>
                     {
-                        respuestas.map((respuesta, indice) => {
-                            return (
-                                <Box data-question={respuesta} data-state='unselected'
-                                 sx={{
-                                    padding: 1,
-                                    margin: 1,
-                                    border: '2px solid blue',
-                                    borderRadius: '10px',
-                                }}
-                                onClick={e => seleccionarRespuesta(e)}
-                                ref={(e) => (boxRefs.current[indice] = e)}
-                            >
-                                <li key={indice}>
-                                {respuesta}
-                                </li>
-                            </Box>
-                            
-                            )
-                        })
+                        respuestas.map((respuesta, position) => (
+                            <Grid item key={position} xs={6}> 
+                                <Paper>
+                                    <Button 
+                                        fullWidth
+                                        variant="contained"
+                                        onClick={comprobarPregunta.bind(this, respuesta)}
+                                    >
+                                        {respuesta}
+                                    </Button>
+                                </Paper>
+                            </Grid>
+                        ))
                     }
-                </ul>
-            </Box>
-            <Box sx={{
-                    padding: 1,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "right",
-                }}
-            >
-                <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                onClick={ e => comprobarPregunta()}
-                >
-                    Comprobar
-                </Button>
+                </Grid>
             </Box>
         </Container>
     </Container>
