b00518a2cdecc9d226e75a7da5531e839eabc503
Yago <uo289549@uniovi.es>
2024-02-29 11:29:13 +0100

    creacion de la clase de test para el juego

---
+++ webapp/src/components/game/Game.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import Game from './Game';
