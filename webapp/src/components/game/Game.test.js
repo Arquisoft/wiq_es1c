ff0d76a1806d51d99eedbeb8af064426e6134333
Yago <uo289549@uniovi.es>
2024-03-01 11:15:23 +0100

    add test

--- webapp/src/components/game/Game.test.js
+++ webapp/src/components/game/Game.test.js
@@ -1,5 +1,26 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router-dom';
-import Game from './Game';
+import {Game} from './Game';
+
+describe('Game Component', () => {
+    it('renders correctly', () => {
+      render(<Game />);
+      expect(screen.getByText(/Cargando pregunta.../i)).toBeInTheDocument();
+    });
+  
+    it('calls startGame method on mount', () => {
+      const startGameSpy = jest.spyOn(Game.prototype, 'startGame');
+      render(<Game />);
+      expect(startGameSpy).toHaveBeenCalled();
+    });
+  
+    it('calls comprobarPregunta method on button click', () => {
+      const comprobarPreguntaSpy = jest.spyOn(Game.prototype, 'comprobarPregunta');
+      render(<Game />);
+      fireEvent.click(screen.getByRole('button', { name: /.../i }));
+      expect(comprobarPreguntaSpy).toHaveBeenCalled();
+    });
+  
+    // Add more test cases as needed
+  });
