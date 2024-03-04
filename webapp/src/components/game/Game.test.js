1bb7af3deff5d90ebb5231120dd2baeabe939a6f
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-03-04 18:22:33 +0100

    Fixed inital tests

--- webapp/src/components/game/Game.test.js
+++ webapp/src/components/game/Game.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import {Game} from './Game';
 
 jest.mock('../../services/game.service', () => ({
-  startNewGame: jest.fn(),
-  nextQuestion: jest.fn().mockResolvedValue({ title: 'Pregunta de prueba', awnsers: ['Respuesta 1', 'Respuesta 2', 'Respuesta 3', 'Respuesta 4'] }),
-  awnser: jest.fn().mockResolvedValue('Respuesta correcta'),
+  startNewGame: () => Promise.resolve(""),
+  nextQuestion: () => Promise.resolve({ title: 'Pregunta de prueba', awnsers: ['Respuesta 1', 'Respuesta 2', 'Respuesta 3', 'Respuesta 4'] }),
+  awnser: () => Promise.resolve('Respuesta 1'),
 }));
+
+jest.spyOn(window, 'alert').mockImplementation(() => {});
 
 describe('Game Component', () => {
 
@@ -16,7 +18,7 @@ describe('Game Component', () => {
     
     await act(async () => {});
 
-    expect(screen.getByText(/Cargando pregunta/i)).toBeInTheDocument();
+    expect(screen.getByText(/Pregunta de prueba/i)).toBeInTheDocument();
 
   });
   
@@ -24,17 +26,15 @@ describe('Game Component', () => {
     render(<Game />);
 
     // Espera a que la pregunta se cargue y se renderice
-    await screen.findByText(/Pregunta de prueba/i);
+    await act(async () => {});
 
     // Simula hacer clic en el botón de respuesta
     fireEvent.click(screen.getByText(/Respuesta 1/i));
 
     // Espera a que se resuelva la promesa y se ejecute la lógica de la función
-    await screen.findByText(/Pregunta de prueba/i);
+    await act(async () => {});
 
-    // Verifica que se haya llamado a la función de respuesta con el valor correcto
-    expect(await screen.findByText(/Respuesta correcta/i)).toBeInTheDocument();
-
+    //TODO: Verify when the display is fancier
     // Verifica que se haya mostrado la alerta adecuada
     expect(window.alert).toHaveBeenCalledWith('Pregunta acertada');
   });
