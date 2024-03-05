41c9c76ea2e5bc35cc6f70a239152e000ef7b6d2
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-03-05 20:23:25 +0100

    History baseline

---
+++ webapp/src/components/history/History.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import {Button, Box, Container, CssBaseline,Typography, Grid, Paper, LinearProgress,} from "@mui/material";
+
+export const History = () => {
+    return (
+        <Container
+            component="main"
+            maxWidth="sm"
+            sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}
+            className="min-h-screen flex justify-center align-middle"
+            >
+                <Container
+                    className="bg-white rounded-lg"
+                    component="main"
+                    maxWidth="sm"
+                >
+                    <CssBaseline />
+
+                    
+
+                </Container>
+        </Container>
+    )
+}
