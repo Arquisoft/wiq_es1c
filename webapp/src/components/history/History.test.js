a1d96c123534db85f59fa3a1ec99f77b0c628643
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-03-06 19:03:05 +0100

    Fixed tests

--- webapp/src/components/history/History.test.js
+++ webapp/src/components/history/History.test.js
@@ -4,7 +4,34 @@ import '@testing-library/jest-dom';
 import {History} from './History';
 
 jest.mock('../../services/user.service', () => ({
-    getHistory: () => ""
+    getHistory: () => Promise.resolve(
+      [
+        {
+          id: "Game 1",
+          Questions: [
+            {onTime: true, answer: "A", user_answer: "A"},
+            {onTime: true, answer: "A", user_answer: "A"},
+            {onTime: true, answer: "A", user_answer: "A"}
+          ]
+        },
+        {
+          id: "Game 2",
+          Questions: [
+            {onTime: true, answer: "A", user_answer: "A"},
+            {onTime: true, answer: "A", user_answer: "A"},
+            {onTime: true, answer: "A", user_answer: "A"}
+          ]
+        },
+        {
+          id: "Game 3",
+          Questions: [
+            {onTime: true, answer: "A", user_answer: "A"},
+            {onTime: true, answer: "A", user_answer: "A"},
+            {onTime: true, answer: "A", user_answer: "A"}
+          ]
+        },
+      ]
+    )
 }));
 
 describe('Game Component', () => {
@@ -13,6 +40,10 @@ describe('Game Component', () => {
     render(<History/>);
     
     await act(async () => {});
+
+    expect(screen.getByText(/Game 1/i)).toBeInTheDocument();
+    expect(screen.getByText(/Game 2/i)).toBeInTheDocument();
+    expect(screen.getByText(/Game 3/i)).toBeInTheDocument();
 
     expect(screen.getByText(/Juego/i)).toBeInTheDocument();
     expect(screen.getByText(/Acertadas/i)).toBeInTheDocument();
