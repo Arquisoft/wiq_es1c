225cf359bb8c80148fb64392e0ca59c8129c7a22
Manuel <gs_manuel@outlook.es>
2024-03-27 19:49:34 +0100

    body of tests

--- webapp/e2e/steps/play-game.steps.js
+++ webapp/e2e/steps/play-game.steps.js
@@ -7,72 +7,87 @@ let page;
 let browser;
 
 defineFeature(feature, test => {
-  
+  const username = "ab"
+  const password = "hr3]pk,UxU=BQp7"
   beforeAll(async () => {
     browser = process.env.GITHUB_ACTIONS
       ? await puppeteer.launch()
       : await puppeteer.launch({ headless: false, slowMo: 1 });
     page = await browser.newPage();
-    //Way of setting up the timeout
     setDefaultOptions({ timeout: 10000 })
 
     await page
-      .goto("http://localhost:3000/login", {
+      .goto("http://localhost:80/login", {
         waitUntil: "networkidle0",
       })
       .catch(() => {});
-  });
+    //Creamos nuevo usuario para los tests
+    await expect(page).toClick("a", {text: "¿No tienes una cuenta? Regístrate"});
+    await expect(page).toFill('input[name="username"]', username);
+    await expect(page).toFill('input[name="password"]', password);
+    await expect(page).toFill('input[name="confirmPassword"]', password);
+    await expect(page).toClick('button', {text: 'Registrarme'});
 
+  });
+  beforeEach(async()=>{
+    await page
+        .goto("http://localhost:80/login", {
+          waitUntil: "networkidle0",
+        })
+        .catch(() => {});
+    await expect(page).toFill('input[name="username"]',username);
+    await expect(page).toFill('input[name="password"]',password);
+    await expect(page).toClick('.MuiButtonBase-root',{
+      visible:true,
+      text:'Iniciar sesión'
+    });
+    await page.waitForNavigation()
+  })
+  afterEach(async()=>{
+    await expect(page).toClick('[data-testid="logout"]',{visible:true});
+  })
+  afterAll(async ()=>{
+    browser.close()
+  })
   test('Starts a new game', ({given,when,then}) => {
-    let username;
-    let password;
 
     given('An logged user since home view', async () => {
-      username = "manu"
-      password = "hr3]pk,UxU=BQp7"
-
-      await expect(page).toClick("a", { text: "¿No tienes una cuenta? Regístrate" });
-      await expect(page).toFill('input[name="username"]', username);
-      await expect(page).toFill('input[name="password"]', password);
-      await expect(page).toFill('input[name="confirmPassword"]', password);
-      await expect(page).toClick('button', { text: 'Registrarme' });
-
+      await page.waitForSelector('h2.MuiTypography-root');
+      const homeElement = await page.$eval('h2.MuiTypography-root', el => el.textContent === 'Home');
+      expect(homeElement).toBeTruthy();
     });
 
     when('I press play', async () => {
-      await expect(page).toClick('button', { text: 'Jugar' })
+      await expect(page).toClick('button > span', { text: 'JUGAR' });
     });
 
     then('A new game starts', async () => {
       const currentUrl = await page.url();
+      //Dar tiempo a que carge
       expect(currentUrl).toBe('http://localhost/game');
-      /*TODO: Add the message
-        await expect(page).toMatchElement("div", { text: "User added successfully" });
-      */
+      await expect(page).toMatchElement('.MuiButtonBase-root', {
+        visible: true
+      });
     });
-  })
-
+  });
   test('The user select one answer', ({given,when,then}) => {
-    let username;
-    let password;
 
     given('A logged user in a game', async () => {
-      username = "pablo"
-      password = "pabloasw"
+      await expect(page).toClick('button > span', { text: 'JUGAR' });
     });
 
     when('I choose an option', async () => {
-      await expect(page).toFill('input[name="username"]', username);
-      await expect(page).toFill('input[name="password"]', password);
-      await expect(page).toFill('input[name="confirmPassword"]', password + 'imdifferent');
-      await expect(page).toClick('button', { text: 'Registrarme' })
+      await expect(page).toClick('[data-buton="btn"]:first-of-type',{visible:true})
     });
-
     then('Show results', async () => {
-        await expect(page).toMatchElement("div", { text: "Las contraseñas no coinciden" });
+      const changedButton = await page.evaluate(() => {
+        const buttons = Array.from(document.querySelectorAll('[data-buton="btn"]'));
+
+        return buttons.some(button => {
+          return button.style.backgroundColor !== 'purple';
+        });
+      });
+      expect(changedButton).toBe(true);
     });
   });
-  afterAll(async ()=>{
-    browser.close()
-  })
 });
