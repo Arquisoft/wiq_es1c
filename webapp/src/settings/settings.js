9b48e72724e18506918b14ff8c29fb2422f48d41
RubenFern <rubenferval@hotmail.com>
2024-03-28 12:01:48 +0100

    added initial settings file for all games mode

---
+++ webapp/src/settings/settings.js
@@ -0,0 +1,24 @@
+export class BasicGame
+{
+    durationQuestion = 10;
+}
+
+export class BatteryOfSages
+{
+    durationQuestion = 60;
+}
+
+export class Discarding
+{
+    durationQuestion = 30;
+}
+
+export class TheHotQuestion
+{
+    durationQuestion = 30;
+}
+
+export class DiscoveringCities
+{
+    durationQuestion = 30;
+}
