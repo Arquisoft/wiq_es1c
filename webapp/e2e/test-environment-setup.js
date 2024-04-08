6756f397b7aea9d1e15947f103fe70fb71309ce0
Manuel <gs_manuel@outlook.es>
2024-04-09 00:29:10 +0200

    Added initial enviroment-setup

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -1,13 +1,16 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-
-
 let auth_service;
+let game_service;
+let question_service;
+let userdetails_serivce;
 
 async function startServer() {
     console.log('Starting auth memory server...');
-
     auth_service = await require("../../auth_service/auth");
+    game_service = await require("../../game_service/game");
+    question_service = await require("../../question_service/question");
+    userdetails_serivce = await require("../../userdetails_serivce/userdetails");
 }
 
 startServer();
