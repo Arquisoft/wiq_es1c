823cf13e6ff25374b348012ed8c4587fd36932e0
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-04-14 13:23:13 +0200

    Can accept a friend request

--- friends_service/friends/endpoints.js
+++ friends_service/friends/endpoints.js
@@ -21,8 +21,18 @@ const sendRequest = async (req, res) => {
         }
     })
 
-    if(request.length > 0) {
-        res.status(400).send({error: "Ya hay una peticion en curso"});
+    
+    let friendship = await Friendship.findAll({
+        where: {
+            [Op.or]: [
+                { friend1: req.body.to, friend2: userId },
+                { friend1: userId, friend2: req.body.to },
+            ]
+        }
+    })
+
+    if(request.length > 0 || friendship.length > 0) {
+        res.status(400).send({error: "Ya hay una peticion en curso, o ya sois amigos"});
         return;
     }
 
@@ -31,13 +41,36 @@ const sendRequest = async (req, res) => {
         to: userId
     })
 
-    if(request != null) {
-        res.status(200).send();
-    }
+    res.status(200).send();
 }
 
 const acceptRequest = async (req, res) => {
+    if(!validate(req, ["to"])) {
+        res.status(400).send();
+        return;
+    }
 
+    const userId = await jwt.verify(req.body.token, privateKey).user_id;
+
+    let request = await FriendRequest.destroy({
+        where: {
+            [Op.or]: [
+                { from: req.body.to, to: userId },
+                { from: userId, to: req.body.to },
+            ]
+        }
+    })
+    
+    if(request) {
+        await Friendship.create({
+            friend1: req.body.to,
+            friend2: userId
+        })
+        
+        res.status(200).send();
+    }
+    
+    res.status(400).send();
 }
 
 const getRequests = async (req, res) => {
