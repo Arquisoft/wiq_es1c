89c7f0b77f4437fc183d14069b68dda957ac9fa2
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-04-14 17:57:27 +0200

    Started tests

---
+++ webapp/src/components/friends/friends.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {render, screen, fireEvent, act, waitFor} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Friends } from './friends';
+import { MemoryRouter, Route } from 'react-router-dom';
+
+jest.mock('../../services/friends.service', () => ({
+    getFriends: () => Promise.resolve([{friend1: "1", friend2: "2"}]),
+    getRequests: () => Promise.resolve([])
+}));
+
+jest.mock('../../services/user.service', () => ({
+    getCurrentUser: () => Promise.resolve("Paco"),
+    getUsers: () => Promise.resolve([{name: "Paco",id:"1"}, {name: "Juan",id:"2"}])
+}));
+
+describe('Game Component', () => {
+    
+  test("renders component",async () => {
+    await act(async () => render(
+      <MemoryRouter>
+          <Friends/>
+      </MemoryRouter>)
+    );
+
+    expect(screen.getByText(/Amigos/i)).toBeInTheDocument();
+    expect(screen.getByText(/Juan/i)).toBeInTheDocument();
+  });
+})
