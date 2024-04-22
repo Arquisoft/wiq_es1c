352813b3736a77595eaa3ce2a612fe0395a30a79
RubenFern <rubenferval@hotmail.com>
2024-04-22 21:37:19 +0200

    added enpoint for get history by user

--- userdetails_service/service/gameService.js
+++ userdetails_service/service/gameService.js
@@ -12,4 +12,23 @@ const getHistory = async (req,res) => {
     res.status(200).send((await response.json()));
 }
 
-module.exports = getHistory;
+const getHistoryByUser = async (req, res) =>
+{
+    const response = await fetch("http://game:8003/api/game/getHistoryByUser", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            token: req.body.token,
+            usedId: req.body.usedId
+        })
+    });
+
+    res.status(200).send(
+        await response.json()
+    );
+}
+
+module.exports = { getHistory, getHistoryByUser };
