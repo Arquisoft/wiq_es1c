9ef39b17c190a73bba8ce16f9df07d0449201a4b
RubenFern <rubenferval@hotmail.com>
2024-04-23 20:22:44 +0200

    added tests

--- webapp/src/components/ranking/Ranking.test.js
+++ webapp/src/components/ranking/Ranking.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, act, fireEvent } from '@testing-library/react';
+import { render, screen, act, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Ranking } from './Ranking';
 import { MemoryRouter } from 'react-router-dom';
