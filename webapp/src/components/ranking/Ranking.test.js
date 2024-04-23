dab7fbf18079f5a44b0e6f0b467077e5f8c121e4
RubenFern <rubenferval@hotmail.com>
2024-04-23 22:43:17 +0200

    addes new user in test

--- webapp/src/components/ranking/Ranking.test.js
+++ webapp/src/components/ranking/Ranking.test.js
@@ -59,6 +59,16 @@ jest.mock('../../services/ranking.service.js', () => (
                             user_answer: '123'
                         }
                     ]
+                },
+                {
+                    user_id: "2",
+                    numberOfQuestions: 1,
+                    Questions: [
+                        {
+                            answer: 'abc',
+                            user_answer: 'abc'
+                        }
+                    ]
                 }
             ]
             
@@ -72,6 +82,13 @@ jest.mock('../../services/ranking.service.js', () => (
                     id: "1",
                     name: "user1"
                 }
+            },
+            {
+                hitsPercentage: 100,
+                user: {
+                    id: "2",
+                    name: "user2"
+                }
             }
         ]
     ),
@@ -83,6 +100,13 @@ jest.mock('../../services/ranking.service.js', () => (
                     id: "1",
                     name: "user1"
                 }
+            },
+            {
+                correctsAnswer: 1,
+                user: {
+                    id: "2",
+                    name: "user2"
+                }
             }
         ]
     ),
@@ -93,6 +117,13 @@ jest.mock('../../services/ranking.service.js', () => (
                 user: {
                     id: "1",
                     name: "user1"
+                }
+            },
+            {
+                numberOfPlays: 1,
+                user: {
+                    id: "2",
+                    name: "user2"
                 }
             }
         ]
@@ -109,8 +140,11 @@ describe('Game Component', () => {
         waitFor(async () => {
             expect(screen.getByText(/Clasificación/i)).toBeInTheDocument();
             expect(screen.getByText(/Usuario: user1/i)).toBeInTheDocument();
+            expect(screen.getByText(/Usuario: user2/i)).toBeInTheDocument();
             expect(screen.getByText(/Acierto: 50 %/i));
+            expect(screen.getByText(/Acierto: 100 %/i));
             expect(screen.getByTestId(/cup-1/i)).toBeInTheDocument();
+            expect(screen.getByTestId(/cup-2/i)).toBeInTheDocument();
 
             screen.getByTestId('select-sort-by').click();
             await act(async () => {});
@@ -124,6 +158,8 @@ describe('Game Component', () => {
             await act(async () => {});
             screen.getByTestId('numberOfCorrectQuestions').click();
             expect(screen.getByText(/Usuario: user1/i)).toBeInTheDocument();
+            expect(screen.getByText(/Usuario: user2/i)).toBeInTheDocument();
+            expect(screen.getByText(/Preguntas correctas: 1/i));
             expect(screen.getByText(/Preguntas correctas: 1/i));
             
             expect(screen.getByText(/Número de preguntas correctas/i)).toBeInTheDocument();
@@ -132,7 +168,9 @@ describe('Game Component', () => {
             await act(async () => {});
             screen.getByTestId('numberOfPlays').click();
             expect(screen.getByText(/Usuario: user1/i)).toBeInTheDocument();
+            expect(screen.getByText(/Usuario: user2/i)).toBeInTheDocument();
             expect(screen.getByText(/Partidas jugadas: 2/i));
+            expect(screen.getByText(/Partidas jugadas: 1/i));
             
             expect(screen.getByText(/Cantidad de partidas/i)).toBeInTheDocument();
 
