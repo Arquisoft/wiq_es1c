b05023271e106ad2aa3d830682b80c534037559c
Manueluz <60448849+Manueluz@users.noreply.github.com>
2024-04-23 17:42:09 +0200

    updage package

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -1,12 +1,8 @@
-const {MongoMemoryServer} = require('mongodb-memory-server');
-let mongoserver;
 let auth_service;
 let game_service;
 let question_service;
 let userdetails_serivce;
 async function startServer() {
-    mongoserver=await MongoMemoryServer.create();
-    process.env.MONGODB_URI=mongoserver.getUri();
     auth_service = await require("../../auth_service/auth");
     game_service = await require("../../game_service/game");
     question_service = await require("../../question_service/question");
